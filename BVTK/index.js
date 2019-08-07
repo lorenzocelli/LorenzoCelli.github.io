398b9c5221c271a9e93109f72a200de34c0abda1
lorenzocelli <celli.lorenzo@yahoo.it>
2019-08-07 19:36:10 +0200

    Cookies

--- BVTK/index.js
+++ BVTK/index.js
@@ -89,13 +89,70 @@ var stick_offset = offset_top(stick_el)-header_offset;
 $(window).on("scroll", stick);
 
 /* ------------------------------------------------------------------------------
-    DO ONCE
+    COOKIES
+    Thanks Gumbo
 ------------------------------------------------------------------------------ */
 
 function do_once(action_id) {
-    var regex = new RegExp("/(?:(?:^|.*;\s*)" + action_id + "\s*\=\s*([^;]*).*$)|^.*$/");
-    if (document.cookie.replace(regex, "$1") !== "true") {
-        alert("Do something here!");
-        document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    var cookie = getCookie(action_id);
+    console.log(cookie);
+    if(!cookie){
+        alert("Hola!");
+        setCookie(action_id, 1, 500);
     }
+}
+
+if (typeof String.prototype.trimLeft !== "function") {
+    String.prototype.trimLeft = function() {
+        return this.replace(/^\s+/, "");
+    };
+}
+if (typeof String.prototype.trimRight !== "function") {
+    String.prototype.trimRight = function() {
+        return this.replace(/\s+$/, "");
+    };
+}
+if (typeof Array.prototype.map !== "function") {
+    Array.prototype.map = function(callback, thisArg) {
+        for (var i=0, n=this.length, a=[]; i<n; i++) {
+            if (i in this) a[i] = callback.call(thisArg, this[i]);
+        }
+        return a;
+    };
+}
+function getCookies() {
+    var c = document.cookie, v = 0, cookies = {};
+    if (document.cookie.match(/^\s*\$Version=(?:"1"|1);\s*(.*)/)) {
+        c = RegExp.$1;
+        v = 1;
+    }
+    if (v === 0) {
+        c.split(/[,;]/).map(function(cookie) {
+            var parts = cookie.split(/=/, 2),
+                name = decodeURIComponent(parts[0].trimLeft()),
+                value = parts.length > 1 ? decodeURIComponent(parts[1].trimRight()) : null;
+            cookies[name] = value;
+        });
+    } else {
+        c.match(/(?:^|\s+)([!#$%&'*+\-.0-9A-Z^`a-z|~]+)=([!#$%&'*+\-.0-9A-Z^`a-z|~]*|"(?:[\x20-\x7E\x80\xFF]|\\[\x00-\x7F])*")(?=\s*[,;]|$)/g).map(function($0, $1) {
+            var name = $0,
+                value = $1.charAt(0) === '"'
+                    ? $1.substr(1, -1).replace(/\\(.)/g, "$1")
+                    : $1;
+            cookies[name] = value;
+        });
+    }
+    return cookies;
+}
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    return getCookies()[name];
 }
